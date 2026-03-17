1198722c55d47d4df7b76341d31c88035136ac64
Carlos3628 <carlosalexandrekadu@gmail.com>
2026-03-17 20:29:29 -0300

    Efetuando os ultimos exercicios.

--- Dart/segundaapl/exercicios2/lib/exercicios2.dart
+++ Dart/segundaapl/exercicios2/lib/exercicios2.dart
@@ -20,4 +20,38 @@ void Exers(){
     5.32 : frutas[0], 2.56 : frutas[1], 7.89 : frutas[2], 3.54 : frutas[3], 9.25 : frutas[4]
   };
   print(precosFrutas);
+
+  print("\n--- Lista de Preços ---");
+  precosFrutas.forEach((preco, nome) {
+    print("Fruta: $nome | Preço: R\$ ${preco.toStringAsFixed(2)}");
+  });
+
+  var filtrarPares = (List<int> numeros) {
+    return numeros.where((n) => n % 2 == 0).toList();
+  };
+
+  List<int> listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  print("\nNúmeros pares: ${filtrarPares(listaNumeros)}");
+
+  enum Pessoa {
+  ana("Ana", 25),
+  joao("João", 17),
+  caio("Caio", 12),
+  beatriz("Beatriz", 19);
+
+  final String nome;
+  final int idade;
+    
+  const Pessoa(this.nome, this.idade);
+}
+
+void verificarMaioridade() {
+  print("\n--- Pessoas Maiores de Idade ---");
+  
+  for (var pessoa in Pessoa.values) {
+    if (pessoa.idade >= 18) {
+      print("${pessoa.nome} tem ${pessoa.idade} anos.");
+    }
+  }
+}
 }
